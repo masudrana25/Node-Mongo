0567f160501c1451ff7bcc60b9ff6826985a0d6c
masudrana25 <mdmasudrana2512000@gmail.com>
2023-07-08 22:24:40 +0600

    Dff

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('I know how to sent node request...YAY!')
+  const fruits = {
+    Product: 'Apple',
+    Price: 2000
+  }
+  res.send(fruits)
+});
+
+app.get('/fruits/banana', (req, res) => {
+  res.send({Fruits : 'Banana', Quantity : 20, Price : 2222})
 })
 
-app.listen(3200, () => console.log(' listening on port 3200'));
+app.listen(3100, () => console.log(' listening on port 3100'));
